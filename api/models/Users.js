4941bf28ee03c552a5e1e9d852e98eaed3334670
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-05-09 14:40:58 +0100

    Updates to documentation

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,6 +1,6 @@
 /**
  * Users.js
- *
+ * @module User Model
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
@@ -27,4 +27,3 @@ module.exports = {
   }
   
 };
-
